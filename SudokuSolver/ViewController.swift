1aa881e2a9b97fe63bfb97d913857458251ba735
Takayoshi Hongan <hongan@MBP2014.local>
2015-05-17 10:20:56 +0900

    Initial Commit

---
+++ SudokuSolver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SudokuSolver
+//
+//  Created by Takayoshi Hongan on 5/17/15.
+//  Copyright (c) 2015 Takayoshi Hongan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
